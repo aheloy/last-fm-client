249d1d2c6ac424a76a61db299a521a0a1739e74b
aheloy <qwsaaswq321123@gmail.com>
2018-02-18 14:15:15 +0500

    Fix api url

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API_KEY = 'd739eec34eda35b701607f170676cef3';
 
-axios.defaults.baseURL = 'http://ws.audioscrobbler.com/2.0/';
+axios.defaults.baseURL = 'https://ws.audioscrobbler.com/2.0/';
 
 axios.interceptors.request.use((config) => {
   config.params.api_key = API_KEY;
