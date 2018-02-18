ee8d019022cdbda89d56cd94684c0109648b1638
aheloy <qwsaaswq321123@gmail.com>
2018-02-18 14:01:07 +0500

    Fix router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { RouterToUrlQuery } from 'react-url-query';
 
 import Root from './containers/root';
